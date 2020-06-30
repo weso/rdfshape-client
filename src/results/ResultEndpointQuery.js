fb32fa711f9dc0aec1bcdc397c01f6b653e87d43
ulitol97 <UO251436@uniovi.es>
2020-06-30 16:00:27 +0200

    Styling and refactoring of DataQuery.js

--- src/results/ResultEndpointQuery.js
+++ src/results/ResultEndpointQuery.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ResultQuery from "./ResultQuery";
 import {Permalink} from '../Permalink';
+import Alert from "react-bootstrap/Alert";
 
 function ResultEndpointInfo(props) {
     return (
         <div>
-        {props.error ? <p>{props.error}</p> : null}
+        {props.error ? <Alert variant='danger'>{props.error}</Alert> : null}
         {props.result ? <div>
                 <ResultQuery result={props.result}/>
         </div> : null
         }
-            <Permalink url={props.permalink}/>
         </div>
     )
 }
