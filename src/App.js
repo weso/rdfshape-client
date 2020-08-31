448708782479e2999a30c78adffa32312ba8f0b1
Jose Emilio Labra Gayo <jelabra@gmail.com>
2020-08-31 14:33:58 +0200

    Small change to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import RDFShapeNavbar from "./RDFShapeNavbar";
 import Container from 'react-bootstrap/Container';
 import Routes from './Routes.js';
-import Cyto from './components/Cyto.js';
+// import Cyto from './components/Cyto.js';
 
 function App() {
 
